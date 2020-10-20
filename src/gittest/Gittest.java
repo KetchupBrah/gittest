fe21ee76d6360e87c7eed55c80f25982fe2407f6
KARATE <mail@mail.com>
2020-10-20 15:21:43 +0700

    ff

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,17 @@
+
+package gittest;
+
+/**
+ *
+ * @author Student
+ */
+public class Gittest {
+
+    
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+}
